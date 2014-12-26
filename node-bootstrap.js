7ed5dd5745c812e3f48fa69ab68e5f4a63a6bc71
Gary Guo <nbdd0121@hotmail.com>
2014-12-26 10:18:26 +0800

    Make unicode categorizer a class called Unicode

--- node-bootstrap.js
+++ node-bootstrap.js
@@ -3,10 +3,11 @@
 var System = require('./external/es6-module-loader').System;
 if (process.argv.length <= 2) {
 	console.log('Argument Required');
-}
-var moduleName = process.argv[2].replace(/\.js$/, '');
-System.import(moduleName).then(function(index) {
+}else{
+	var moduleName = process.argv[2].replace(/\.js$/, '');
+	System.import(moduleName).then(function(index) {
 
-}).catch(function(err) {
-	console.log(err.stack ? err.stack : err);
-})
+	}).catch(function(err) {
+		console.log(err.stack ? err.stack : err);
+	})
+}
---
+++ syntax/unicode/Unicode.js
@@ -0,0 +1,46 @@
+import {smpIdStart, smpIdContinue, bmpTable} from 'syntax/unicode/CategoryTable';
+
+const compressMap = "01234567";
+
+function tableLookup(table, codepoint) {
+	let i = 0;
+	while (true) {
+		if (codepoint < table[i++]) {
+			return false;
+		}
+		if (codepoint <= table[i++]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+class Unicode {
+	static isIdStart(codepoint) {
+		if (typeof(codepoint) == 'string') {
+			codepoint = codepoint.codePointAt(0);
+		}
+		if (codepoint >= 65535)
+			return tableLookup(smpIdStart, codepoint);
+		else
+			return !!(compressMap.indexOf(bmpTable[codepoint]) & 1);
+	}
+	static isIdContinue(codepoint) {
+		if (typeof(codepoint) == 'string') {
+			codepoint = codepoint.codePointAt(0);
+		}
+		if (codepoint >= 65535)
+			return tableLookup(smpIdContinue, codepoint);
+		else
+			return !!(compressMap.indexOf(bmpTable[codepoint]) & 2);
+	}
+	static isSpace(codepoint) {
+		if (typeof(codepoint) == 'string') {
+			codepoint = codepoint.codePointAt(0);
+		}
+		if (codepoint >= 65535)
+			return false;
+		else
+			return !!(compressMap.indexOf(bmpTable[codepoint]) & 4);
+	}
+}
